46c24bd0961426d2d3512d81eb3e1be4dd3c67ef
unknown <dannysessom@gmail.com>
2023-01-06 22:29:28 -0600

    changes

--- main.js
+++ main.js
@@ -1,7 +1,79 @@
-function scrollToTop() {
-  const anchorHome = document.getElementById("header")
-  anchorHome.scrollIntoView({behavior: "smooth"})
-}
+
+  let curScrollPos = 0
+  let oldScrollPos = 0
+
+window.addEventListener('scroll', function(){
+  // console.log(this.scrollY)
+
+  let isScrolledNav = false
+
+
+  //HTML Elements
+  const nav = document.querySelector('.nav-wrapper')
+  
+  if ( window.pageYOffset > curScrollPos ) {
+      nav.classList.add("scroll")
+      isScrolledNav = true
+  }
+
+  oldScrollPos = curScrollPos
+  console.log("oldScrollPos " + oldScrollPos)
+  curScrollPos = this.scrollY
+  console.log("curScrollPos " + curScrollPos)
+  console.log("window y " + this.scrollY)
+
+  if (oldScrollPos < curScrollPos) {
+    nav.classList.add("scroll")
+  } else if (oldScrollPos > curScrollPos) {
+    nav.classList.remove("scroll")
+  }
+
+
+})
+
+// window.onscroll = function () {
+
+//   let isScrolledNav = false
+
+//   let curScrollPos = window.screenY
+//   let oldScrollPos = 0
+
+//   //HTML Elements
+//   const nav = document.querySelector('.nav-wrapper')
+  
+//   if ( window.pageYOffset > curScrollPos ) {
+//       nav.classList.add("scroll")
+//       isScrolledNav = true
+//   }
+
+//   function onScroll() {
+//     // oldScrollPos = curScrollPos
+//     console.log("oldScrollPos " + oldScrollPos)
+//     curScrollPos = window.screenY
+//     console.log("curScrollPos " + curScrollPos)
+//     console.log("window y " + window.screenY)
+
+//     if (oldScrollPos < curScrollPos) {
+//       nav.classList.add("scroll")
+//     } else if (oldScrollPos > curScrollPos) {
+//       nav.classList.remove("scroll")
+//     }
+
+//     // if ( window.pageYOffset > curScrollPos) {
+//     //   nav.classList.add("scroll")
+//     //   isScrolledNav = true
+//     // }   else if ( window.pageYOffset <= curScrollPos ) {
+//     //   nav.classList.remove("scroll")
+      
+//     //   isScrolledNav = false
+//     // }
+//   }
+//   onScroll ()
+//   return onScroll
+
+// }()
+
+
 
 function scrollToVideos() {
   const anchorHome = document.getElementById("videos")
