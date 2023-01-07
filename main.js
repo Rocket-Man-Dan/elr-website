03e0c4a1b9af4ee45ba914c895aa607bdb3b0091
unknown <dannysessom@gmail.com>
2023-01-06 19:02:27 -0600

    added youtube videos and more styling

---
+++ main.js
@@ -0,0 +1,9 @@
+function scrollToTop() {
+  const anchorHome = document.getElementById("header")
+  anchorHome.scrollIntoView({behavior: "smooth"})
+}
+
+function scrollToVideos() {
+  const anchorHome = document.getElementById("videos")
+  anchorHome.scrollIntoView({behavior: "smooth"})
+}
