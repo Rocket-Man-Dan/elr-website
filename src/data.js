d97a48fb11036285d733c242a3b80b06603fbfe1
unknown <dannysessom@gmail.com>
2023-01-18 13:38:11 -0600

    more stuff

---
+++ src/data.js
@@ -0,0 +1,71 @@
+let shopItemsData = [{
+    id: "hoodie_elr_main_black",
+    name: "Black Hoodie - ELR Original",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/hoodie_ELR_main_black.png"
+},
+{
+    id: "hoodie_elr_cars_black",
+    name: "Black Hoodie - ELR Cars",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/hoodie_ELR_cars_black.png"
+},
+{
+    id: "hoodie_elr_monogram_black",
+    name: "Black Hoodie - Monogram",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/hoodie_ELR_monogram_black.png"
+},
+{
+    id: "hoodie_elr_main_purple",
+    name: "Purple Hoodie - ELR Original",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/hoodie_ELR_main_purple.png"
+},
+{
+    id: "hoodie_elr_cars_purple",
+    name: "Purple Hoodie - ELR Cars",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/hoodie_ELR_cars_purple.png"
+},
+{
+    id: "snapback_elr_main_black",
+    name: "Black Snapback - ELR Original",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/snapback_ELR_main_black.png"
+},
+{
+    id: "snapback_elr_cars_black",
+    name: "Black Snapback - ELR Cars",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/snapback_ELR_cars_black.png"
+},
+{
+    id: "snapback_elr_monogram_black",
+    name: "Black Snapback - ELR Monogram",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/snapback_ELR_monogram_black.png"
+},
+{
+    id: "shirt_elr_main_purple",
+    name: "Purple T-Shirt - ELR Original",
+    price: 20,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/tshirt_ELR_main_purple.png"
+},
+{
+    id: "shirt_elr_cars_purple",
+    name: "Purple T-Shirt - ELR Cars",
+    price: 20,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "../src/img/merch/tshirt_ELR_cars_purple.png"
+}
+]
---
+++ src/main.js
@@ -0,0 +1,123 @@
+
+// ---------- NAV BAR ---------- //
+  let curScrollPos = 0
+  let oldScrollPos = 0
+
+window.addEventListener('scroll', function(){
+  let isScrolledNav = false
+
+  //HTML Elements
+  const nav = document.querySelector('.nav-wrapper')
+  
+  if ( window.pageYOffset > curScrollPos ) {
+      nav.classList.add("scroll")
+      isScrolledNav = true
+  }
+
+  oldScrollPos = curScrollPos
+  // console.log("oldScrollPos " + oldScrollPos)
+  curScrollPos = this.scrollY
+  // console.log("curScrollPos " + curScrollPos)
+  // console.log("window y " + this.scrollY)
+
+  if (oldScrollPos < curScrollPos) {
+    nav.classList.add("scroll")
+  } else if (oldScrollPos > curScrollPos) {
+    nav.classList.remove("scroll")
+  }
+})
+
+function scrollToVideos() {
+  const anchorHome = document.getElementById("videos")
+  anchorHome.scrollIntoView({behavior: "smooth"})
+}
+
+
+// ---------- STORE ---------- //
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+  
+  if (search === undefined){
+    basket.push ({
+      id: selectedItem.id,
+      item: 1,
+    })
+  }
+  else{
+    search.item += 1;
+  }
+  // console.log(selectedItem.id)
+  update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket))
+} 
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x)=>x.id === selectedItem.id);
+
+  if (search === undefined) return
+
+  if (search.item === 0) return;
+    else{
+      search.item -= 1;
+    }
+    // console.log(selectedItem.id)
+    update(selectedItem.id);
+    basket = basket.filter((x) => x.item !== 0)
+    localStorage.setItem("data", JSON.stringify(basket))
+}
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id)
+  document.getElementById(id).innerHTML = search.item
+  // console.log(search.item)
+  calculation()
+}
+
+let calculation = () => {
+  let cartAmount = document.getElementById('cartAmount');
+  cartAmount.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, 0);
+  // console.log(basket.map((x) => x.item))
+  
+}
+
+// if on store page generate items
+let store = document.getElementById('store-wrapper')
+let basket = JSON.parse(localStorage.getItem("data")) || []
+let isInStore = document.getElementById('store')
+
+if (isInStore !== null){
+  let generateStore = () => {
+    return (store.innerHTML= shopItemsData.map((x)=>{
+      let {id, name, price, desc, img} = x
+      let search = basket.find((x) => x.id === id) || [];
+      return `
+      <div id=product-id-${id} class="item">
+      <div class="details">
+        <div class="item-text">
+          <img class="item-img" src="${img}" alt="">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+        </div>
+        <div class="price-quantity">
+          <h2>$${price}</h2>
+          <div class="item-buttons">
+            <img onclick="decrement(${id})" class="item-plus-minus" src="../src/img/icons/icon_minus.svg" alt="">
+            <div id=${id} class="quantity">
+            ${search.item === undefined? 0: search.item}
+            </div>
+            <img onclick="increment(${id})" class="item-plus-minus" src="../src/img/icons/icon_plus.svg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+      `
+    }).join(""));
+  };
+  
+  generateStore()
+  
+  
+  calculation()
+}
