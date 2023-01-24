ed1b1f569b4af8119ecd949f10f04f40fc39d1fa
unknown <dannysessom@gmail.com>
2023-01-23 20:07:04 -0600

    nav overhaul, charity

--- src/main.js
+++ src/main.js
@@ -1,98 +1,120 @@
-
 // ---------- NAV BAR ---------- //
-  let curScrollPos = 0
-  let oldScrollPos = 0
+let curScrollPos = 0;
+let oldScrollPos = 0;
+let toTopPos = 200;
 
-window.addEventListener('scroll', function(){
-  let isScrolledNav = false
+window.addEventListener("scroll", function () {
+  let isScrolledNav = false;
 
   //HTML Elements
-  const nav = document.querySelector('.nav-wrapper')
-  
-  if ( window.pageYOffset > curScrollPos ) {
-      nav.classList.add("scroll")
-      isScrolledNav = true
+  const nav = document.querySelector(".nav-wrapper");
+  const toTop = this.document.querySelector(".back-to-top");
+  let checkbox = document.getElementById("mobile-nav");
+
+  if (window.pageYOffset > curScrollPos) {
+    nav.classList.add("scroll");
+    isScrolledNav = true;
   }
 
-  oldScrollPos = curScrollPos
+  if (window.pageYOffset > toTopPos) {
+    toTop.classList.add("scroll");
+  } else if (window.pageYOffset < toTopPos) {
+    toTop.classList.remove("scroll");
+  }
+
+  oldScrollPos = curScrollPos;
   // console.log("oldScrollPos " + oldScrollPos)
-  curScrollPos = this.scrollY
+  curScrollPos = this.scrollY;
   // console.log("curScrollPos " + curScrollPos)
-  // console.log("window y " + this.scrollY)
+  console.log("window y " + this.scrollY);
 
   if (oldScrollPos < curScrollPos) {
-    nav.classList.add("scroll")
+    nav.classList.add("scroll");
   } else if (oldScrollPos > curScrollPos) {
-    nav.classList.remove("scroll")
+    nav.classList.remove("scroll");
   }
-})
+
+  if (checkbox.checked) {
+    nav.classList.remove("scroll");
+  }
+});
+
+document.addEventListener("click", function ClickOutsideNav(event) {
+  const nav = document.querySelector(".mobile-nav-hitbox");
+  let checkbox = document.getElementById("mobile-nav");
+
+  if (checkbox.checked) {
+    if (nav.contains(event.target)) {
+      checkbox.checked = false;
+      console.log("clicked");
+    }
+  }
+});
 
 function scrollToVideos() {
-  const anchorHome = document.getElementById("videos")
-  anchorHome.scrollIntoView({behavior: "smooth"})
+  const anchorHome = document.getElementById("videos");
+  anchorHome.scrollIntoView({ behavior: "smooth" });
 }
-
 
 // ---------- STORE ---------- //
 let increment = (id) => {
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
-  
-  if (search === undefined){
-    basket.push ({
+
+  if (search === undefined) {
+    basket.push({
       id: selectedItem.id,
       item: 1,
-    })
-  }
-  else{
+    });
+  } else {
     search.item += 1;
   }
   // console.log(selectedItem.id)
   update(selectedItem.id);
-    localStorage.setItem("data", JSON.stringify(basket))
-} 
+  localStorage.setItem("data", JSON.stringify(basket));
+};
 
 let decrement = (id) => {
   let selectedItem = id;
-  let search = basket.find((x)=>x.id === selectedItem.id);
+  let search = basket.find((x) => x.id === selectedItem.id);
 
-  if (search === undefined) return
+  if (search === undefined) return;
 
   if (search.item === 0) return;
-    else{
-      search.item -= 1;
-    }
-    // console.log(selectedItem.id)
-    update(selectedItem.id);
-    basket = basket.filter((x) => x.item !== 0)
-    localStorage.setItem("data", JSON.stringify(basket))
-}
+  else {
+    search.item -= 1;
+  }
+  // console.log(selectedItem.id)
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
 
 let update = (id) => {
-  let search = basket.find((x) => x.id === id)
-  document.getElementById(id).innerHTML = search.item
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
   // console.log(search.item)
-  calculation()
-}
+  calculation();
+};
 
 let calculation = () => {
-  let cartAmount = document.getElementById('cartAmount');
-  cartAmount.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, 0);
+  let cartAmount = document.getElementById("cartAmount");
+  cartAmount.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
   // console.log(basket.map((x) => x.item))
-  
-}
+};
 
 // if on store page generate items
-let store = document.getElementById('store-wrapper')
-let basket = JSON.parse(localStorage.getItem("data")) || []
-let isInStore = document.getElementById('store')
+let store = document.getElementById("store-wrapper");
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+let isInStore = document.getElementById("store");
 
-if (isInStore !== null){
+if (isInStore !== null) {
   let generateStore = () => {
-    return (store.innerHTML= shopItemsData.map((x)=>{
-      let {id, name, price, desc, img} = x
-      let search = basket.find((x) => x.id === id) || [];
-      return `
+    return (store.innerHTML = shopItemsData
+      .map((x) => {
+        let { id, name, price, desc, img } = x;
+        let search = basket.find((x) => x.id === id) || [];
+        return `
       <div id=product-id-${id} class="item">
       <div class="details">
         <div class="item-text">
@@ -105,19 +127,19 @@ if (isInStore !== null){
           <div class="item-buttons">
             <img onclick="decrement(${id})" class="item-plus-minus" src="../src/img/icons/icon_minus.svg" alt="">
             <div id=${id} class="quantity">
-            ${search.item === undefined? 0: search.item}
+            ${search.item === undefined ? 0 : search.item}
             </div>
             <img onclick="increment(${id})" class="item-plus-minus" src="../src/img/icons/icon_plus.svg" alt="">
           </div>
         </div>
       </div>
     </div>
-      `
-    }).join(""));
+      `;
+      })
+      .join(""));
   };
-  
-  generateStore()
-  
-  
-  calculation()
+
+  generateStore();
+
+  calculation();
 }
